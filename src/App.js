18505db58fc7a7446f6968beb91959fc087320f8
chaitanya <sparkscj234@gmail.com>
2022-09-09 21:45:23 +0530

    "router added"

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import Navbar from "./components/navbar/Navbar";
-import {useTheme} from './helpers/useTheme'
+import { useTheme } from './helpers/useTheme'
 import Home from "./pages/home/Home";
 import Details from "./pages/details/Details";
-import { Routes ,Route } from 'react-router-dom';
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
 
 function App() {
-  const {changeTheme} = useTheme()
+  const { changeTheme } = useTheme()
   return (
-    <div className="App" style={{backgroundColor:changeTheme("background"),color:changeTheme("text")}}>
-        <Navbar />
+    <div className="App" style={{ backgroundColor: changeTheme("background"), color: changeTheme("text") }}>
+      <Navbar />
+      <Router>
 
-          <Routes>
-            <Route exact path='/find-country' element={<Home/>}/>
-            <Route exact path="/find-country/:countryName" element={<Details/>} />
-          </Routes>
-      
-      </div>
+        <Routes>
+          <Route exact path='/find-country' element={<Home />} />
+          <Route exact path="/find-country/:countryName" element={<Details />} />
+        </Routes>
+      </Router>
+
+    </div>
   )
 }
 
