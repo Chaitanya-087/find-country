67a34569c1738c690300b16119b122f8785f30ed
chaitanya <sparkscj234@gmail.com>
2022-09-09 21:35:34 +0530

    "router removed"

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Navbar from "./components/navbar/Navbar";
 import {useTheme} from './helpers/useTheme'
 import Home from "./pages/home/Home";
 import Details from "./pages/details/Details";
-import { BrowserRouter as Router,Routes ,Route } from 'react-router-dom';
+import { Routes ,Route } from 'react-router-dom';
 
 
 
@@ -12,12 +12,10 @@ function App() {
     <div className="App" style={{backgroundColor:changeTheme("background"),color:changeTheme("text")}}>
         <Navbar />
 
-          <Router>
-            <Routes>
-              <Route exact path='/' element={<Home/>}/>
-              <Route exact path="/:countryName" element={<Details/>} />
-            </Routes>
-          </Router>
+          <Routes>
+            <Route exact path='/' element={<Home/>}/>
+            <Route exact path="/:countryName" element={<Details/>} />
+          </Routes>
       
       </div>
   )
